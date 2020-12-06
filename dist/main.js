f88efe075a8e392c22f7f917166b7351f2056b7d
Mahmoud Bakr <mbakr6821@gmail.com>
2020-12-06 20:22:52 +0200

    setup webpack configurations file with build and test

---
+++ dist/main.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('Let us GO!');
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
+    }
+};
